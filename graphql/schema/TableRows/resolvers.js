bfc5468deb6e20d77d2089bb9fedf989a2ee601d
adhe <adwidianjaya@gmail.com>
2019-09-24 14:45:45 +0700

    bug fix

--- graphql/schema/TableRows/resolvers.js
+++ graphql/schema/TableRows/resolvers.js
@@ -2,15 +2,14 @@ const resolvers = {
   Query: {
     allRowsByTableAndKeyspace: async (self, params, context) => {
       const results = await context.cassandra.execute(
-        `SELECT * FROM ${params.keyspace_name}.${params.table_name} LIMIT 100;`
-          .keyspace_name,
+        `SELECT * FROM ${params.keyspace_name}.${params.table_name} LIMIT 100;`,
         [],
         {
           keyspace: params.keyspace_name
         }
       );
       return results.rows.map(result => JSON.stringify(result));
-    }, 
+    },
     countRowsByTableAndKeyspace: async (self, params, context) => {
       const results = await context.cassandra.execute(
         `SELECT COUNT(*) FROM ${params.keyspace_name}.${params.table_name};`,
